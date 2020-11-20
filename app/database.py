579dd657854191886ec577e159398f6bc80a53a9
n1EzeR <erasylz@gmail.com>
2020-11-20 20:53:22 +0600

    Init videos table
    Add timestamps for users table

--- app/database.py
+++ app/database.py
@@ -1,5 +1,16 @@
 from databases import Database
-from sqlalchemy import Binary, Boolean, Column, MetaData, String, Table, create_engine
+from sqlalchemy import (
+    Binary,
+    Boolean,
+    Column,
+    DateTime,
+    ForeignKey,
+    MetaData,
+    String,
+    Table,
+    create_engine,
+    func,
+)
 from sqlalchemy.dialects.postgresql import UUID
 
 from .config import DATABASE_URL
@@ -18,6 +29,21 @@ users = Table(
     Column("password", Binary),
     Column("is_active", Boolean, server_default="true"),
     Column("is_superuser", Boolean, server_default="false"),
+    Column("created_at", DateTime, server_default=func.now()),
+    Column("updated_at", DateTime, onupdate=func.now()),
+    Column("deleted_at", DateTime, default=None),
+)
+
+videos = Table(
+    "videos",
+    metadata,
+    Column("id", UUID, primary_key=True),
+    Column("file_path", String, unique=True),
+    Column("owner", ForeignKey("users.id", ondelete="CASCADE")),
+    Column("is_deleted", Boolean, server_default="false"),
+    Column("created_at", DateTime, server_default=func.now()),
+    Column("updated_at", DateTime, onupdate=func.now()),
+    Column("deleted_at", DateTime, default=None),
 )
 
 metadata.create_all(engine)
